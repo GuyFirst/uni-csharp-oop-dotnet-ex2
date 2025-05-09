7a296bafe57a429d169f01b5b3d3c7edb0cd5c04
GuyFirst <guyfirst19@gmail.com>
2025-05-10 02:40:26 +0300

    init commit

---
+++ Ex02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Ex02
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
