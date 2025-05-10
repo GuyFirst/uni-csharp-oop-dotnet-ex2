ddad5af35ece0a34a6362a5415726153e5fbd94f
GuyFirst <guyfirst19@gmail.com>
2025-05-10 15:27:14 +0300

    started working on GuessAttempt

--- Ex02/GuessAttempt.cs
+++ Ex02/GuessAttempt.cs
@@ -2,8 +2,23 @@
 
 namespace Ex02
 {
-    public class Guess
+    public class GuessAttempt
     {
+        public string SecretCode { get; set; }
+        public string FeedbackResult { get; set; } // the [v,x,vv] kind of stuff
 
+        public GuessAttempt(string i_SecretCode)
+        {
+            SecretCode = i_SecretCode;
+        }
+
+        public string GiveFeedbackOnGuess(string i_UserGuess)
+        {
+
+
+
+
+            return FeedbackResult;
+        }
     }
 }
