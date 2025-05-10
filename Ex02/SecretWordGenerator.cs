6ff8da2eb8d2343b4635c87b7312b9fd9d36ea5e
ROYS-WIN-PC <royda2@mta.ac.il>
2025-05-10 14:15:00 +0300

    finished SecretWordGenerator class

--- Ex02/SecretWordGenerator.cs
+++ Ex02/SecretWordGenerator.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ex02
 {
-    public class SecretWordGenerator    //ROY
+    public class SecretWordGenerator //ROY
     {
+        private readonly Random r_RandomGenerator = new Random();
+        private readonly char[] r_CharsBank = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+        private const short k_SecretWordLength = 4;     //remember CONST MEANS #DEFINE IN CSHARP
 
+        public string GenerateSecretWord()
+        {
+            string secretWord = string.Empty;
+            List<char> availableChars = new List<char>(r_CharsBank);
+
+            for (short i = 0; i < k_SecretWordLength; i++)
+            {
+                int randomIndex = r_RandomGenerator.Next(availableChars.Count);
+                secretWord += availableChars[randomIndex];
+                availableChars.RemoveAt(randomIndex);
+            }
+
+            return secretWord;
+        }
+        
     }
 }
