c70d81f5c338853327b78ea285c764d5098d8aa1
GuyFirst <guyfirst19@gmail.com>
2025-05-11 13:49:29 +0300

    gamemangerstructure

--- Ex02/GameManager.cs
+++ Ex02/GameManager.cs
@@ -5,5 +5,55 @@ namespace Ex02
 {
     public class GameManager
     {
+        public bool QuittingGameFlag { get; set; }
+
+        public void StartNewGame()
+        {
+            while (true)
+            {
+                runGame();
+                
+                //getting the response from consoleUI if the user wants to play a new game
+
+                if(QuittingGameFlag)
+                {
+                    return;
+                }
+            }
+        }
+        
+        
+        private void runGame()
+        {
+            //create game state
+
+
+
+            //gameloop
+            {
+
+                // if user enters q - quit the game
+
+
+
+            }
+
+          
+            //return
+
+
+
+        }
+
+
+
+
+
+
+
     }
+
+
+    
+     
 }
